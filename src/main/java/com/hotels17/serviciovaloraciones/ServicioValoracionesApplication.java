cdeaf1336966aa2351e8377266233970a1f6f891
Yera <yera.gonzalez.landa@zabalburu.org>
2022-03-02 12:39:18 +0100

    Initial commit

---
+++ src/main/java/com/hotels17/serviciovaloraciones/ServicioValoracionesApplication.java
@@ -0,0 +1,13 @@
+package com.hotels17.serviciovaloraciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioValoracionesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicioValoracionesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hotels17/serviciovaloraciones/ServicioValoracionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotels17.serviciovaloraciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioValoracionesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
